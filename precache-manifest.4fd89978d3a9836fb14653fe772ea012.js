1ffe87bdf94de8db74f3689b02dd1f93b0ca395d
kano-kenji <aleksandrtarasevich77@gmail.com>
2020-07-23 11:54:29 -0600

    Updates

---
+++ precache-manifest.4fd89978d3a9836fb14653fe772ea012.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cea9545c83d3a803742df44eda0a80e",
+    "url": "/counters/index.html"
+  },
+  {
+    "revision": "4fafe00fe1b06d81d8d9",
+    "url": "/counters/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "175e0a261c9956674679",
+    "url": "/counters/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4fafe00fe1b06d81d8d9",
+    "url": "/counters/static/js/2.dbc77605.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counters/static/js/2.dbc77605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175e0a261c9956674679",
+    "url": "/counters/static/js/main.3c2fa204.chunk.js"
+  },
+  {
+    "revision": "a64d93fc1d448c302c58",
+    "url": "/counters/static/js/runtime-main.95d00a74.js"
+  }
+]);
